6fcf35a66d1a3d291b0ba0a5e95282c389b2c967
Tereza Burianova <40072706+T-ess@users.noreply.github.com>
2021-03-17 19:11:10 +0100

    Added more FKs

--- test.sql
+++ test.sql
@@ -7,7 +7,9 @@ DROP TABLE Kriminalni_cinnost CASCADE CONSTRAINTS ;
 DROP TABLE Uzemi CASCADE CONSTRAINTS ;
 DROP TABLE Objednavka CASCADE CONSTRAINTS ;
 DROP TABLE R_Ucast_Na_Setkani CASCADE CONSTRAINTS;
-
+DROP TABLE R_Clen_Cinnost CASCADE CONSTRAINTS;
+--TODO: u FK omezeni je lepe explicitne deklarovat ON DELETE na NO ACTION nebo SET DEFAULT, aby nedoslo ke smazani produktu pri mazani kategorie
+--TODO: unique a default
 CREATE TABLE Don (
     Alias varchar(255) PRIMARY KEY,
     Jmeno varchar(255) NOT NULL ,
@@ -16,7 +18,7 @@ CREATE TABLE Don (
 );
 
 CREATE TABLE Familie(
-  Nazev varchar(255) PRIMARY KEY,
+  Nazev_familie varchar(255) PRIMARY KEY,
   Pocet_clenu int CHECK(Pocet_clenu>=0),
   Alias varchar(255) NOT NULL,
   CONSTRAINT Vedouci_FK FOREIGN KEY (Alias) REFERENCES Don(Alias)
@@ -26,9 +28,15 @@ CREATE TABLE Uzemi(
     ID_uzemi int PRIMARY KEY ,
     Ulice varchar(255) NOT NULL ,
     Mesto varchar(255) NOT NULL ,
-    PSC int NOT NULL,  --TODO: doplnit check pre 5 miestny int
+    PSC number NOT NULL CHECK ( length(to_char(PSC)) = 5 ),
     Rozloha int NOT NULL ,
-    GPS varchar(255) CHECK(REGEXP_LIKE(GPS, '^([0-9]|[1-8][0-9]|[9][0]) ([N]|[S]), ([0-9]|[1-9][0-9]|[1][0-7][0-9]|[1][8][0]) ([E]|[W])$'))
+    GPS varchar(255) CHECK(REGEXP_LIKE(GPS, '^([0-9]|[1-8][0-9]|[9][0]) ([N]|[S]), ([0-9]|[1-9][0-9]|[1][0-7][0-9]|[1][8][0]) ([E]|[W])$')),
+
+    Nazev_familie varchar(255) DEFAULT NULL,
+    CONSTRAINT Familie_uzemi_FK FOREIGN KEY (Nazev_familie) REFERENCES Familie(Nazev_familie),
+    Spada_od DATE DEFAULT NULL,
+    Spada_do DATE DEFAULT NULL
+    --TODO: historie - spada pod od-do
 );
 
 CREATE TABLE Setkani_Donu(
@@ -47,17 +55,24 @@ CREATE TABLE  R_Ucast_Na_Setkani(
 );
 
 CREATE TABLE Radovy_clen(
-    ID_clena int PRIMARY KEY NOT NULL,
+    ID_clena int PRIMARY KEY,
     Jmeno varchar(255) NOT NULL,
     Datum_narozeni varchar(255) NOT NULL ,
-    Datum_prijeti varchar(255)
+    Datum_prijeti varchar(255),
+    Nazev_familie varchar(255) NOT NULL,
+
+    CONSTRAINT Familie_FK FOREIGN KEY (Nazev_familie) REFERENCES Familie(Nazev_familie),
+    Role varchar(255) NOT NULL,
+    Role_od DATE NOT NULL,
+    Role_do DATE NOT NULL
 );
 
 CREATE TABLE Aliance(
-  ID_aliance int PRIMARY KEY  NOT NULL,
+  ID_aliance int PRIMARY KEY,
   Stav varchar(255) CHECK (Stav = 'aktivni' or Stav = 'pozastavena' or Stav = 'zrusena') NOT NULL ,
   Datum_zalozeni DATE,
   Datum_ukonceni DATE
+    --TODO: vztah aliance a familie
 );
 
 CREATE TABLE Kriminalni_cinnost(
@@ -72,15 +87,34 @@ CREATE TABLE Kriminalni_cinnost(
     --Ostatni operace
     Druh varchar(255),
 
+    ID_aliance int,
+    Nazev_familie varchar(255),
+
+    CONSTRAINT Vedouci_Aliance_FK FOREIGN KEY (ID_aliance) REFERENCES Aliance(ID_aliance),
+    CONSTRAINT Vedouci_Familie_FK FOREIGN KEY (Nazev_familie) REFERENCES Familie(Nazev_familie),
+
+    ID_uzemi int NOT NULL,
+    CONSTRAINT Uzemi_Cinnost_FK FOREIGN KEY (ID_uzemi) REFERENCES Uzemi(ID_uzemi),
+
     CHECK (
-        (Type = 'Vrazda' and Obet is not NULL and Misto is not NULL and Druh is NULL) or
-        (type = 'Ostatni operace' and Druh is not NULL and Obet is NULL and Misto is NULL)
+        ((ID_aliance is not NULL and Nazev_familie is NULL) or
+        (ID_aliance is NULL and Nazev_familie is not NULL)) and
+        ((Type = 'Vrazda' and Obet is not NULL and Misto is not NULL and Druh is NULL) or
+        (type = 'Ostatni operace' and Druh is not NULL and Obet is NULL and Misto is NULL))
     )
 );
 
+CREATE TABLE  R_Clen_Cinnost(
+    Jmeno_operace varchar(255) NOT NULL,
+    ID_clena int NOT NULL,
+    Role varchar(255) NOT NULL,
+    CONSTRAINT Clen_FK FOREIGN KEY (ID_clena) REFERENCES Radovy_clen(ID_clena),
+    CONSTRAINT Cinnost_FK FOREIGN KEY (Jmeno_operace) REFERENCES Kriminalni_cinnost(Jmeno_operace)
+    --TODO: clen musi patrit do familie vedouci cinnost
+);
 
 CREATE TABLE Objednavka(
-  ID_objednavky int PRIMARY KEY NOT NULL ,
+  ID_objednavky int PRIMARY KEY,
   Datum DATE,
   Stav varchar(255) CHECK (Stav = 'vykonana' or Stav = 'pozastavena' or Stav = 'zrusena' or Stav = 'v procesu') NOT NULL,
   Alias varchar(255) NOT NULL,
@@ -98,26 +132,32 @@ VALUES ('Veduci','Jano','2.6.2000', 39);
 INSERT INTO Don
 VALUES ('Zabijak','Jozo','23.5.1980', 43);
 
-INSERT INTO Familie(Nazev, Pocet_clenu, Alias)
+INSERT INTO Familie(Nazev_familie, Pocet_clenu, Alias)
 VALUES ('Jedlickovi', 23, 'Veduci');
-INSERT INTO Familie(Nazev, Pocet_clenu, Alias)
+INSERT INTO Familie(Nazev_familie, Pocet_clenu, Alias)
 VALUES ('Kolarovi', 130, 'Zabijak');
 
+INSERT INTO Uzemi(ID_uzemi, Ulice, Mesto, PSC, Rozloha, GPS, Nazev_familie, Spada_od, Spada_do)
+VALUES(1,'Sedmikraskova', 'Brno', 95501, 35, '34 N, 180 W', 'Kolarovi', TO_DATE( '2020-03-01', 'YYYY-MM-DD' ), TO_DATE( '2020-05-01', 'YYYY-MM-DD' ));
 INSERT INTO Uzemi(ID_uzemi, Ulice, Mesto, PSC, Rozloha, GPS)
-VALUES(1,'Sedmikraskova', 'Brno', 95501, 35, '34 N, 180 W');
+VALUES (2, 'Pampeliskova', 'Cesky Krumlov', 45477, 64, '39 N, 120 W');
+
 
 INSERT INTO Setkani_Donu(ID_setkani, Datum_cas, Misto, ID_uzemi)
 VALUES (1, TO_DATE( '2020-03-01 15:15', 'YYYY-MM-DD HH24:MI' ), 'sklep u Joza', 1);
 
-INSERT INTO Radovy_clen(ID_clena, Jmeno, Datum_narozeni, Datum_prijeti)
-VALUES(1,'Jozo','12.3.1990', null);
-INSERT INTO Radovy_clen(ID_clena, Jmeno, Datum_narozeni, Datum_prijeti)
-VALUES(2,'Jan','24.11.2000', '4.8.2021');
+INSERT INTO Radovy_clen(ID_clena, Jmeno, Datum_narozeni, Datum_prijeti, Nazev_familie, Role, Role_od, Role_do)
+VALUES(1,'Jozo','12.3.1990', null, 'Jedlickovi', 'zastupce Dona', TO_DATE( '2020-03-01', 'YYYY-MM-DD' ), TO_DATE( '2020-05-01', 'YYYY-MM-DD' ));
+INSERT INTO Radovy_clen(ID_clena, Jmeno, Datum_narozeni, Datum_prijeti, Nazev_familie, Role, Role_od, Role_do)
+VALUES(2,'Jan','24.11.2000', '4.8.2021', 'Kolarovi', 'cistic bot', TO_DATE( '2020-03-01', 'YYYY-MM-DD' ), TO_DATE( '2020-05-01', 'YYYY-MM-DD' ));
 
-INSERT INTO Kriminalni_cinnost(Jmeno_operace, Doba_trvani, Stav, Datum, Type, Obet, Misto, Druh)
-VALUES('Zabitie Fera', '2 dni', 'vykonana', TO_DATE( '2020-03-01 15:15', 'YYYY-MM-DD HH24:MI' ), 'Vrazda', 'Jozo', 'sklep', NULL);
-INSERT INTO Kriminalni_cinnost(Jmeno_operace, Doba_trvani, Stav, Datum, Type, Obet, Misto, Druh)
-VALUES('Okradnutie Fera', '1 den', 'vykonana', TO_DATE( '2020-03-01 15:15', 'YYYY-MM-DD HH24:MI' ), 'Ostatni operace', NULL, NULL, 'Kradez');
+INSERT INTO Aliance(id_aliance, stav, datum_zalozeni, datum_ukonceni)
+VALUES (1, 'aktivni', TO_DATE( '2020-03-01 15:15', 'YYYY-MM-DD HH24:MI' ), TO_DATE( '2020-05-01 15:15', 'YYYY-MM-DD HH24:MI' ));
+
+INSERT INTO Kriminalni_cinnost(jmeno_operace, doba_trvani, stav, datum, type, obet, misto, druh, id_aliance, Nazev_familie, id_uzemi)
+VALUES('Zabitie Fera', '2 dni', 'vykonana', TO_DATE( '2020-03-01 15:15', 'YYYY-MM-DD HH24:MI' ), 'Vrazda', 'Jozo', 'sklep', NULL, NULL, 'Jedlickovi', 1);
+INSERT INTO Kriminalni_cinnost(jmeno_operace, doba_trvani, stav, datum, type, obet, misto, druh, id_aliance, Nazev_familie, id_uzemi)
+VALUES('Okradnutie Fera', '1 den', 'vykonana', TO_DATE( '2020-03-01 15:15', 'YYYY-MM-DD HH24:MI' ), 'Ostatni operace', NULL, NULL, 'Kradez', 1, NULL, 1);
 
 
 INSERT INTO  Objednavka(ID_objednavky, Datum, Stav, Alias, Jmeno_operace)
@@ -125,4 +165,7 @@ VALUES(1, TO_DATE( '2020-03-01 15:15', 'YYYY-MM-DD HH24:MI' ), 'vykonana', 'Vedu
 
 INSERT INTO  R_Ucast_Na_Setkani(Alias, ID_setkani)
 VALUES ('Veduci', 1);
+
+INSERT INTO R_Clen_Cinnost(jmeno_operace, id_clena, role)
+VALUES ('Okradnutie Fera', 2, 'ridic');
 ------------------------------------------------------
